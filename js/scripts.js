b24d7c9b3f89a8a234af41deced5c42b3c2a41b7
Anand Angalig and Lisa MacCarrigan <lisa@>
2016-08-10 10:01:11 -0400

    add JS variables for user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var job1Input = $("input#job1").val();
+    var job2Input = $("input#job2").val();
+    var skill1Input = $("input#skill1").val();
+    var skill2Input = $("input#skill2").val();
+    var product1Input = $("input#product1").val();
+    var product2Input = $("input#product2").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
